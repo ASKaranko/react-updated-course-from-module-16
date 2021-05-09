d504351d9d8a763e62b9c0d075ca2602d17448e8
Karanko Andrei <andr.karanko@gmail.com>
2021-05-09 19:50:29 +0300

    added implementation of custom hook in forms

--- forms-module-16/src/components/SimpleInput.js
+++ forms-module-16/src/components/SimpleInput.js
@@ -1,113 +1,64 @@
-import { useEffect, useRef, useState } from "react";
+import useInput from "../hooks/use-input";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
-  const [enteredName, setEnteredName] = useState("");
-  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput
+  } = useInput((value) => value.trim() !== "");
 
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput
+  } = useInput((value) => {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return pattern.test(value);
+  });
 
-  // Если несколько полей input, то нужно ввести дополнительное состояние
-  // const [formIsValid, setFormIsValid] = useState(false);
-
-  // Сделано переменной вместо переменной состояния, так как мы можем
-  // ее получить как производную enteredName и touched
-  const enteredNameIsValid = enteredName.trim() !== "";
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
-
-  const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  const enteredEmailIsValid = pattern.test(enteredEmail);
-  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
-
-  // Вместо переменной общего состояния формы formIsValid и внедрения
-  // useEffect можно использовать только переменную
   let formIsValid = false;
 
-  // useEffect(() => {
   if (enteredNameIsValid && enteredEmailIsValid) {
-    // setFormIsValid(true);
     formIsValid = true;
   }
-  // else {
-  // 	// setFormIsValid(false);
-  // }
-  // }, [enteredNameIsValid]);
-
-  // Использование state для input или ref зависит от того, нужно
-  // ли делать проверку валидации с каждым stroke для валидации или же нужно
-  // просто получить значение input, например, при submit
-  const nameInputChangeHandler = (event) => {
-    setEnteredName(event.target.value);
-    // setEnteredName выполняет обновление не сразу, поэтому
-    // используется в if проверка на event.target.value
-
-    // Проверка выполняется переменной enteredNameIsValid выше
-    // if (event.target.value.trim() !== "") {
-    // 	setEnteredNameIsValid(true);
-    // }
-  };
-
-  const emailInputChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-  }
-
-  const nameInputBlurHandler = (event) => {
-    setEnteredNameTouched(true);
-    // if (enteredName.trim() === "") {
-    // 	setEnteredNameIsValid(false);
-    // }
-  };
-
-  const emailInputBlurHandler = (event) => {
-    setEnteredEmailTouched(true);
-  };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    setEnteredNameTouched(true);
-    setEnteredEmailTouched(true);
 
     if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
-    // setEnteredNameIsValid(true);
-
-    console.log(enteredName);
-    // const enteredValue = nameInputRef.current.value;
-    // console.log(enteredValue);
-    setEnteredName("");
-    setEnteredEmail("");
-    setEnteredNameTouched(false);
-    setEnteredEmailTouched(false);
-    // альтернатива сброса input с помощью ref - это непосредственное
-    // обновление DOM и это неправильно, так как React должен это делать
-    // nameInputRef.current.value = '';
+    resetNameInput();
+    resetEmailInput();
   };
 
-  const nameInputClasses = nameInputIsInvalid
+  const nameInputClasses = nameInputHasError
     ? "form-control invalid"
     : "form-control";
 
-  const emailInputClasses = emailInputIsInvalid
-      ? "form-control invalid"
-      : "form-control";
+  const emailInputClasses = emailInputHasError
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
-          onChange={nameInputChangeHandler}
-          onBlur={nameInputBlurHandler}
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
           value={enteredName}
         />
-        {nameInputIsInvalid && (
+        {nameInputHasError && (
           <p className="error-text">Name must not be empty</p>
         )}
       </div>
@@ -116,11 +67,11 @@ const SimpleInput = (props) => {
         <input
           type="email"
           id="email"
-          onChange={emailInputChangeHandler}
-          onBlur={emailInputBlurHandler}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
           value={enteredEmail}
         />
-        {emailInputIsInvalid && (
+        {emailInputHasError && (
           <p className="error-text">Email must be in right format</p>
         )}
       </div>
